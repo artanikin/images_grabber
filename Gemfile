696f7ffb43a521bd92513ab4ccc7270223e8c566
Anikin Artyom <anikinartyom@gmail.com>
2013-08-02 13:49:14 +0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.12'
+gem 'jquery-rails'
+gem 'sqlite3'
+gem 'nokogiri'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'turn', '0.8.2', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImagesGrabber::Application.config.secret_token = 'a1a4a896a731dcd74068ebb7a312349885493c1934e6b62e37697f4eab4aa05d151496b9b385717c56d1b3ad016c830c506182ae8c53f7b6d49b04101b08daca'
