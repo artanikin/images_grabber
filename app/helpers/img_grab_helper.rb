2a5508c2da49526d79159a3af7ea32e0177ae9b2
Anikin Artyom <anikinartyom@gmail.com>
2013-08-09 11:28:49 +0600

    Changed display of trophies

--- app/helpers/img_grab_helper.rb
+++ app/helpers/img_grab_helper.rb
@@ -1,2 +1,10 @@
 module ImgGrabHelper
+  def create_link(dir, image)
+    content = []
+
+    content << image_tag("/assets/downloaded/#{@dir}/#{image}")
+    content << content_tag(:span, image)
+
+    content.join.html_safe
+  end
 end
